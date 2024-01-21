12efc3e6de96d703d9c3c6aa8776f5366ffbd8ca
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2024-01-21 19:54:20 -0300

    working webflow (need to validate mobile envs!)

--- app.vue
+++ app.vue
@@ -1,5 +1,101 @@
 <template>
-  <NuxtLayout>
+  <NuxtLayout v-if="loggedIn">
     <NuxtPage />
   </NuxtLayout>
+  <div class="hemocione-login-loading-wrapper" v-else>
+    <NuxtImg src="/logos/principal-horizontal.svg" class="logo" />
+    <ElButton
+      v-if="attemptedAutoLogin && !loggedIn"
+      @click="doLogin"
+      type="primary"
+      size="large"
+    >
+      Entrar
+    </ElButton>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { Browser } from "@capacitor/browser";
+const loggedIn = ref(false);
+const attemptedAutoLogin = ref(false);
+const config = useRuntimeConfig();
+const urlToken = useRoute().query.token;
+useHead({ title: "Hemocione" });
+
+const evaluateCurrentLogin = async () => {
+  const currentUserCookie = useCookie(config.public.authLocalKey);
+  const currentUserLocalStorage = localStorage.getItem(
+    config.public.authLocalKey
+  );
+
+  const currentUserToken = currentUserCookie.value || currentUserLocalStorage;
+  console.log("currentUserToken", currentUserToken);
+  if (currentUserToken) {
+    try {
+      const { data }: { data: Ref<{ token: string; user: any }> } =
+        await useFetch(
+          `${config.public.hemocioneIdApiUrl}/users/regenerate-token`,
+          {
+            headers: {
+              Authorization: `Bearer ${currentUserToken}`,
+            },
+          }
+        );
+      const newToken = data.value.token;
+      localStorage.setItem(config.public.authLocalKey, newToken);
+      loggedIn.value = true;
+    } catch (e) {
+      console.error(e);
+    } finally {
+      attemptedAutoLogin.value = true;
+    }
+    if (attemptedAutoLogin.value && !loggedIn.value) {
+      localStorage.removeItem(config.public.authLocalKey);
+    }
+  }
+};
+
+Browser.addListener("browserFinished", async () => {
+  await evaluateCurrentLogin();
+});
+
+const doLogin = async () => {
+  await Browser.open({
+    url: encodeURI(
+      `${config.public.hemocioneIdUrl}/?redirect=${window.location.href}`
+    ),
+    windowName: "_self",
+    toolbarColor: "#bb0a08",
+  });
+};
+
+if (urlToken) {
+  localStorage.setItem(config.public.authLocalKey, String(urlToken));
+  loggedIn.value = true;
+  window.history.replaceState({}, document.title, window.location.pathname);
+} else {
+  await evaluateCurrentLogin();
+  if (!loggedIn.value) {
+    await doLogin();
+    await evaluateCurrentLogin();
+  }
+}
+</script>
+
+<style scoped>
+.hemocione-login-loading-wrapper {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  gap: 3em;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+}
+
+.logo {
+  width: 80%;
+  height: auto;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,32 @@
 import { defineNuxtConfig } from "nuxt/config";
 
+const getSiteUrl = () => {
+  if (process.env.VERCEL_ENV === "development") {
+    return "https://app.d.hemocione.com.br";
+  }
+
+  if (process.env.VERCEL_ENV === "preview") {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  if (process.env.VERCEL_ENV === undefined) {
+    return "http://localhost:3000";
+  }
+
+  return "https://app.hemocione.com.br";
+};
+
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       hemocioneIdApiUrl:
         process.env.NUXT_HEMOCIONE_ID_API_URL ||
         "https://hemocione-id-dev.cpt.hemocione.com.br",
+      hemocioneIdUrl:
+        process.env.NUXT_HEMOCIONE_ID_URL || "https://id.d.hemocione.com.br",
+      authLocalKey: process.env.NUXT_AUTH_LOCAL_KEY || "devHemocioneId",
+      userDataLocalKey: "userData",
+      siteUrl: getSiteUrl(),
     },
   },
   ssr: false,
@@ -30,5 +51,8 @@ export default defineNuxtConfig({
     alias: {
       cdn: "https://cdn.hemocione.com.br",
     },
+  },
+  nitro: {
+    preset: "vercel",
   },
 });
